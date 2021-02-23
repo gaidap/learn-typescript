2c938f1e521046cc6923010d2c03a625cd4a71b1
gaidap <pc.gaida@gmail.com>
2021-02-23 09:13:04 +0100

    generate fake User data

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,6 +1,6 @@
 import faker from 'faker';
 
-class User {
+export class User {
   fullName: string;
   location: {
     lat: number;
@@ -8,6 +8,10 @@ class User {
   };
 
   constructor() {
-
+    this.fullName = `${faker.name.firstName()} ${faker.name.lastName()}`;
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Yup, I\'m up!');
+// use curly brackets if you want to import a specific entity from a file
+// without curly braces the library has to declare a default export.
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
