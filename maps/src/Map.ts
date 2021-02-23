6999b94845c8bb94a2ceee2dc84ec0bb85851825
gaidap <pc.gaida@gmail.com>
2021-02-23 16:58:09 +0100

    refactoring with interfaces

--- maps/src/Map.ts
+++ maps/src/Map.ts
@@ -1,14 +1,36 @@
+import { User } from './User';
+import { Company } from './Company';
+
+interface Mappable {
+  location: {
+    lat: number,
+    lng: number
+  }
+}
+
 export class Map {
   googleMap: google.maps.Map;
 
-  constructor() {
-    this.initNewMap();
+  constructor(elementId: string) {
+    this.initNewMap(elementId);
   }
 
-  // Quick and simple solution to hide api key with dotenv
-  private initNewMap() {
+  addMarker(newMappable: Mappable): void {
+    this.addMarkerDelayed((mappable: Mappable, map: google.maps.Map): void => {
+      new google.maps.Marker({
+        map: map,
+        position: {
+          lat: mappable.location.lat,
+          lng: mappable.location.lng
+        }
+      });
+    }, newMappable);
+  }
+
+  // Quick and simple solution to wait for google maps api to load in benefit of hiding the api key with dotenv
+  private initNewMap(elementId: string): void {
     try {
-      this.googleMap = new google.maps.Map(document.getElementById('map'), {
+      const newCustomMap = new google.maps.Map(document.getElementById(elementId), {
         backgroundColor: 'white',
         zoom: 1,
         center: {
@@ -16,12 +38,20 @@ export class Map {
           lng: 0
         }
       });
+      this.googleMap = newCustomMap;
     } catch (error) {
       if (error.message.includes('google is not defined')) {
-        setTimeout(this.initNewMap, 10);
+        setTimeout(this.initNewMap, 100, elementId);
       } else {
         throw error;
       }
     };
+  }
+  private addMarkerDelayed(delayedFn: Function, mappable: Mappable) {
+    if (this.googleMap !== undefined) {
+      delayedFn(mappable, this.googleMap);
+    } else {
+      setTimeout(this.addMarkerDelayed, 100, delayedFn, mappable);
+    }
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,9 +1,12 @@
 // use curly brackets if you want to import a specific entity from a file
 // without curly braces the library has to declare a default export.
-import { User } from './User';
 import { Company } from './Company';
 import { Map } from './Map';
+import { User } from './User';
 
-const customMap = new Map();
+const customMap = new Map('map');
 
-
+const user = new User();
+customMap.addMarker(user);
+const company = new Company();
+customMap.addMarker(company);
