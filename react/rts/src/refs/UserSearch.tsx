06a861cb6a7ba8f37d1c2968085241b4dde76528
gaidap <pc.gaida@gmail.com>
2021-02-24 14:50:59 +0100

    learning refs

--- react/rts/src/refs/UserSearch.tsx
+++ react/rts/src/refs/UserSearch.tsx
@@ -1,6 +1,5 @@
-import { strict } from 'node:assert';
-import { stringify } from 'node:querystring';
-import React, { useState } from 'react';
+// You can use this style of component writing for quick prototyping
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
   { name: 'Paul', age: 23 },
@@ -10,8 +9,13 @@ const users = [
 ];
 
 const UserSearch: React.FC = () => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
   const [name, setName] = useState('');
   const [user, setUser] = useState<{ name: string; age: number } | undefined>();
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
 
   const onClick = () => {
     const foundUser = users.find((user) => {
@@ -23,16 +27,15 @@ const UserSearch: React.FC = () => {
       console.log(`User ${name} not found.`);
     }
     setUser(foundUser);
+    inputRef.current?.focus();
   };
 
   return <div>
     User Search
-    <input value={name} onChange={(e) => setName(e.target.value)} />
+    <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
     <button onClick={onClick}>Find User</button>
     <div>
-      {user?.name}
-      <br />
-      {user?.age}
+      {user?.name} {user && `(${user.age})`}
     </div>
   </div>;
 };
