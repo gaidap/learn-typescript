378f4ed5d618d63b5737e4bcb7de1554eae8b717
gaidap <pc.gaida@gmail.com>
2021-03-03 21:39:13 +0100

    NPM Package search example app finished

--- redux/redux-ts/src/components/RepositoriesList.tsx
+++ redux/redux-ts/src/components/RepositoriesList.tsx
@@ -22,6 +22,9 @@ const RepositoriesList: React.FC = () => {
         <input value={term} onChange={onChangeHandler} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {!error && loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((name) => <div key={name}>{name}</div>)}
     </div>
   );
 };
