41f57ae4db4bf09398cd80704d3b0a61cab4d3cd
gaidap <pc.gaida@gmail.com>
2021-03-03 21:31:55 +0100

    create typed selector

--- redux/redux-ts/src/components/RepositoriesList.tsx
+++ redux/redux-ts/src/components/RepositoriesList.tsx
@@ -1,9 +1,11 @@
-import {useState} from 'react';
-import {useActions} from '../hooks/useActions';
+import { useState } from 'react';
+import { useSelector } from '../hooks/useTypedSelector';
+import { useActions } from '../hooks/useActions';
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('');
   const { searchRepositories } = useActions();
+  const { data, error, loading } = useSelector((state) => state.repositories);
 
   const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(e.target.value);
@@ -14,12 +16,14 @@ const RepositoriesList: React.FC = () => {
     searchRepositories(term);
   };
 
-  return (<div>
-    <form onSubmit={onSubmitHandler}>
-      <input value={term} onChange={onChangeHandler}/>
-      <button>Search</button>
-    </form>
-  </div>);
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input value={term} onChange={onChangeHandler} />
+        <button>Search</button>
+      </form>
+    </div>
+  );
 };
 
 export default RepositoriesList;
