02b08fd1df59c7bd7d9397db714dd7634df3de30
gaidap <pc.gaida@gmail.com>
2021-03-03 21:02:15 +0100

    making requests

--- redux/redux-ts/src/components/RepositoriesList.tsx
+++ redux/redux-ts/src/components/RepositoriesList.tsx
@@ -1,10 +1,26 @@
+import {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {actionCreators} from '../state';
+
 const RepositoriesList: React.FC = () => {
-return (<div>
-  <form>
-    <input/>
-    <button>Search</button>
-  </form>
-</div>);
+  const [term, setTerm] = useState('');
+  const dispatch = useDispatch();
+
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(e.target.value);
+  };
+
+  const onSubmitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch(actionCreators.searchRepositories(term));
+  };
+  
+  return (<div>
+    <form onSubmit={onSubmitHandler}>
+      <input value={term} onChange={onChangeHandler}/>
+      <button>Search</button>
+    </form>
+  </div>);
 };
 
 export default RepositoriesList;
